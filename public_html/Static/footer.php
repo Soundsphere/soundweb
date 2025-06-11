40902b63fb391e0e167d7e2a41dd44e382ce4d25
soundsphere <beru@soundsphere.net>
2025-06-11 18:12:44 +0200

    Increase footer icons size
    Increased sice from 18 to 20 px for the icons in the footer

--- public_html/Static/footer.php
+++ public_html/Static/footer.php
@@ -2,9 +2,9 @@
   <div class="smimages">
     <p>
       <a rel="me" href="https://social.tchncs.de/@Soundsphere" target="_blank">
-      <img src="/Static/mastodon.png" alt="mastodon icon" style="width:18px;height:18px;"></a>
+      <img src="/Static/mastodon.png" alt="mastodon icon" style="width:20px;height:20px;"></a>
       <a href="https://www.instagram.com/soundsphere/" target="_blank">
-      <img src="/Static/instagram.png" alt="instagram icon" style="width:18px;height:18px;"></a>
+      <img src="/Static/instagram.png" alt="instagram icon" style="width:20px;height:20px;"></a>
     </p>
   </div>
 </div>
