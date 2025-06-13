79571914b99c05d311b3188dc4e40b87b61308bc
soundsphere <beru@soundsphere.net>
2025-06-14 01:06:33 +0200

    Added Fingerboarding
    Added the fingerboarding link to the header so these pages are accessible

--- public_html/Static/header.php
+++ public_html/Static/header.php
@@ -2,4 +2,5 @@
     <a href="/">Home</a>
     <a href="/HTML/cds.html">CDs</a>
     <a href="/HTML/modular.html">Modular</a>
+    <a href="/fingerboarding/fingerboarding.thml">Fingerboarding</a>
 </div>
