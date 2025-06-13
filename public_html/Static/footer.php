ed6b6e76a65a6cd063e2185c632de2c27c95bcf6
soundsphere <beru@soundsphere.net>
2025-06-13 23:51:03 +0200

    Added youtube to the icons
    youtube icon has been addded to the footer

--- public_html/Static/footer.php
+++ public_html/Static/footer.php
@@ -5,6 +5,8 @@
       <img src="/Static/mastodon.png" alt="mastodon icon" style="width:20px;height:20px;"></a>
       <a href="https://www.instagram.com/soundsphere/" target="_blank">
       <img src="/Static/instagram.png" alt="instagram icon" style="width:20px;height:20px;"></a>
+      <a href="https://www.youtube.com/@sndsphr" target="_blank">
+      <img src="/Static/youtube.png" alt="youtube icon" style="width:20px;height:20px;"></a>
     </p>
   </div>
 </div>
